49048c9bf668f37d1ea1a6aa987e4303868d237e
csawyer4 <56616969+csawyer4@users.noreply.github.com>
2020-01-16 16:52:58 -0800

    Step 1 done
    Added code to the different files as instructed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
+var map = L.map('map').fitWorld();
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3Nhd3llcjQiLCJhIjoiY2syc2g2bnlzMGtyeDNubW55bHRzNnkzOSJ9.WZ63B0C4v437JrMyODN-6g', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v10',
+}).addTo(map);
