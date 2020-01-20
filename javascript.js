6c6fb07da25de51da2323870db6a5b8a5a4272a5
csawyer4 <56616969+csawyer4@users.noreply.github.com>
2020-01-19 17:55:52 -0800

    Alert Window
    Put in alert to say site is using location. And other small details.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpzbGFnZXIiLCJhIjoiZUMtVjV1ZyJ9.2uJjlUi0OttNighmI-8ZlQ', {
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3Nhd3llcjQiLCJhIjoiY2syc2g2bnlzMGtyeDNubW55bHRzNnkzOSJ9.WZ63B0C4v437JrMyODN-6g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id:'mapbox/light-v10',
@@ -6,7 +6,7 @@ var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
     zoomOffset: -1,
 });
 
-var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpzbGFnZXIiLCJhIjoiZUMtVjV1ZyJ9.2uJjlUi0OttNighmI-8ZlQ', {
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3Nhd3llcjQiLCJhIjoiY2syc2g2bnlzMGtyeDNubW55bHRzNnkzOSJ9.WZ63B0C4v437JrMyODN-6g', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id:'mapbox/dark-v10',
@@ -15,13 +15,7 @@ var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
 });
 
 var map = L.map('map', {layers:[light]}).fitWorld();
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3Nhd3llcjQiLCJhIjoiY2syc2g2bnlzMGtyeDNubW55bHRzNnkzOSJ9.WZ63B0C4v437JrMyODN-6g', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v10',
-//     tileSize: 512,
-//     zoomOffset: -1,
-// }).addTo(map);
+
 
 function onLocationFound(e) {
     var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
@@ -35,9 +29,22 @@ function onLocationFound(e) {
       }
       else {
           L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
-      }}
+      }
+      var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+      var sunrise = times.sunrise.getHours();
+      var sunset = times.sunset.getHours();
 
 
+      var currentTime = new Date().getHours();
+          if (sunrise < currentTime && currentTime < sunset){
+            map.removeLayer(dark);
+            map.addLayer(light);
+          }
+          else {
+            map.removeLayer(light);
+            map.addLayer(dark);
+          }
+}
 function onLocationError(e) {
   alert(e.message);
 }
@@ -46,3 +53,5 @@ map.on('locationfound', onLocationFound);//this is the event listener
 map.on('locationerror', onLocationError);
 
 map.locate({setView: true, maxZoom: 16});
+
+window.alert("Alert\nThis site wants to use your location data to show you where you are on a map. This data is also being used to display what time of day it is and whether the sun is up or down to customize the basemap to day/night mode. Your location data will not be stored or shared with anyone. To block/unblock this site from using your location data, go to your browser settings.")
